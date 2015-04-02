2f28c5425d506b286a88600d860ed62e30066f1b
Jimmy102306089 <102306089@nccu.edu.tw>
2015-04-03 00:13:33 +0800

    message

---
+++ README.md
@@ -0,0 +1,1 @@
+https://murmuring-tundra-7436.herokuapp.com
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402143930) do
+ActiveRecord::Schema.define(version: 20150402160611) do
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
